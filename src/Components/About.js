50f5f4471810b63be4d2dfa5f378929407aa5eef
rstott87 <rstott87@yahoo.com>
2022-02-20 19:15:09 -0500

    finished about me

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,12 +3,17 @@ import React from 'react'
 function About (props) {
     return (
         <div className="aboutMe">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-             fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-             culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Hello! I’m a front-end web developer out of Boston, MA. In my personal experience, learning is best done in a hands-on interactive 
+            environment. With that in mind, a lot of my work focuses on
+            designing and building UIs that demonstrate, visualize, and simplify more complex concepts 
+            through basic user interactions. The majority of this work 
+            is done using only React and CSS. 
+            </p> 
+             <p> <br></br>
+                In the Winter of 2022, I completed a full-time six month web development program
+                at Bloom Institute of Technology which was one of the most challenging but also most rewarding 
+                experiences in my software development journey. 
+             </p>
         </div>
         
     )
